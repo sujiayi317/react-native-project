47a6e12a77352de6de10578725f376eddba37b2e
Jiayi <sjy.su@mail.utoronto.ca>
2021-06-24 08:40:28 -0400

    Lists and ScrollView

--- App.js
+++ App.js
@@ -1,29 +1,33 @@
-import React, { useState } from 'react';
-import { StyleSheet, Text, View, TextInput } from 'react-native';
+import React, { useState } from "react";
+import { StyleSheet, Text, View, ScrollView } from "react-native";
 
 export default function App() {
-  const [name, setName] = useState('shawn');
-  const [age, setAge] = useState('30')
-  
+  const [people, setPeople] = useState([
+    { name: "shawn", key: "1" },
+    { name: "yoshi", key: "2" },
+    { name: "mario", key: "3" },
+    { name: "luigi", key: "4" },
+    { name: "peach", key: "5" },
+    { name: "toad", key: "6" },
+    { name: "boser", key: "7" },
+  ]);
+
   return (
     <View style={styles.container}>
-      <Text>Enter name:</Text>
-      <TextInput 
-        // multiline
-        style={styles.input} 
-        placeholder='e.g. John Doe' 
-        // onChangeText={(val) => setName(val)} />
-        onChangeText={setName} />
-
-      <Text>Enter age:</Text>
-      <TextInput 
-        keyboardType='numeric'
-        style={styles.input} 
-        placeholder='e.g. 99' 
-        onChangeText={(val) => setAge(val)} />
-        
-      <Text>name: {name}, age: {age}</Text>
-
+      <ScrollView>
+        {/* {people.map((item) => {
+          return (
+            <View key={item.key}>
+              <Text style={styles.item}>{item.name}</Text>
+            </View>
+          );
+        })} */}
+        {people.map((item) => (
+          <View key={item.key}>
+            <Text style={styles.item}>{item.name}</Text>
+          </View>
+        ))}
+      </ScrollView>
     </View>
   );
 }
@@ -31,15 +35,16 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    paddingTop: 40,
+    paddingHorizontal: 20,
+    // alignItems: 'center',
+    // justifyContent: 'center',
   },
-  input: {
-    borderWidth: 1,
-    borderColor: '#777',
-    padding: 8,
-    margin: 10,
-    width:200
-  }
+  item: {
+    marginTop: 24,
+    padding: 30,
+    backgroundColor: "pink",
+    fontSize: 24,
+  },
 });
